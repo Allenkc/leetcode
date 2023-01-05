2599b27bb3394d8d7f3898985b3d026b2f9e8f2c
allenkc <aa87220@gmail.com>
2023-01-05 21:53:45 +0800

    [add] P125 Valid Palindrome

---
+++ src/main/java/leetcode/editor/en/ValidPalindrome.java
@@ -0,0 +1,29 @@
+package leetcode.editor.en;
+
+//P125_Valid Palindrome
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right){
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }
+            while(left < right && !Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }
+
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
